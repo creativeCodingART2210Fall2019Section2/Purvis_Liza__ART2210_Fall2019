eeee0df98137edb72191a58c7ecb6fcf0d0608af
Apurvi4 <55360729+Apurvi4@users.noreply.github.com>
2019-10-19 13:53:51 -0500

    Add files via upload

---
+++ Purvis_Liza_ART2210_Minigame_Fall2019/Shapeshiftersketch.js
@@ -0,0 +1,13 @@
+function preload (){
+    img=loadImage('projects/Purvis_Liza_ART2210_Minigame_Fall2019/png sequence stationary')
+}
+
+
+
+function setup() {
+canvas (windowWidth, windowHeight)
+}
+
+function draw() {
+  image (img,50,50)
+}
